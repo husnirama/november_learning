5e4796e3ac8375a5b722da38751b9e95262f4294
husnirama <husniramandalubis@gmail.com>
2025-11-17 12:09:54 +0700

    adding advance function js

--- task1.js
+++ task1.js
@@ -1,22 +1,33 @@
 function sumArray(arr){
-    var numbers = arr;
     let sum = 0;
-    numbers.forEach(element => {
-        sum += element;
+    arr.forEach(num => {
+        sum += num;
     });
-    return sum
+    return sum;
 }
 const arr = [1,2,3,4,5];
 console.log(sumArray(arr));
 
+
+// advance using reduce
+const sumArr = arr => arr.reduce((a,b) => a + b, 0);
+console.log(sumArr(arr));
+
 function reverseString(str){
-    var string = str;
-    var len_str = string.length;
+    const len_str = str.length;
     let reverse = "";
     for (let i = 1; i <= len_str; i++){
-        reverse += string[len_str - i];
+        reverse += str[len_str - i];
     }
-    return reverse
+    return reverse;
 }
+
+
 const word = "husni ramanda lubis";
 console.log(reverseString(word));
+
+let reversed = "";
+for (const char of word){
+    reversed = char + reversed;
+}
+console.log(reversed);
