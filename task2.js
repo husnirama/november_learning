162451a17ce2e781c5726c20a0d098bf5dd876bc
husnirama <husniramandalubis@gmail.com>
2025-11-18 13:52:30 +0700

    hard challenge solved

--- task2.js
+++ task2.js
@@ -87,14 +87,128 @@ console.log(evenSquare(arrayChallenge));
 
 
 
-const evenSquareTop50 = arr =>
-    arr.reduce((acc, num) => {
-        if (num % 2 === 0) {
-            const square = num ** 2;
-            if (square > 50) acc.push(square);
-        }
-        return acc;
-    }, []);
+// const evenSquareTop50 = arr =>
+//     arr.reduce((acc, num) => {
+//         if (num % 2 === 0) {
+//             const square = num ** 2;
+//             if (square > 50) acc.push(square);
+//         }
+//         return acc;
+//     }, []);
 
-const arrayChallenge2 = [1, 10, 3, 25, 4, 6];
-console.log(evenSquareTop50(arrayChallenge2));
+// const arrayChallenge2 = [1, 10, 3, 25, 4, 6];
+// console.log(evenSquareTop50(arrayChallenge2));
+
+// const longName = arr =>
+//     arr
+//     .filter(name => name.length > 5)
+//     .map(name => name.toUpperCase());
+
+
+
+// const longName = arr =>
+//     arr.reduce((acc, name) =>{
+//         if (name.length > 5){
+//             const upperName = name.toUpperCase();
+//             acc.push(upperName);
+//         }
+//         return acc
+//     }, []);
+
+
+// const names = ["husni", "ramanda", "ani", "rafi", "jlubis"]
+// console.log(longName(names));
+
+// const topBuyers = arr =>
+const users = [
+    { name: "husni", purchases: [10, 50, 20] },
+    { name: "ani", purchases: [5] },
+    { name: "ramanda", purchases: [100, 20, 30, 50] },
+    { name: "ani", purchases: [15, 25, 30] },     // duplicate name
+    { name: "rafi", purchases: [5, 10, 15] },
+    { name: "ani", purchases: [10] }              // duplicate name again
+];
+
+const topBuyers = arr =>
+    Object.entries(
+    arr
+    .reduce((acc, user) => {
+        if (!acc[user.name]) {
+            acc[user.name] = [];
+        }
+        acc[user.name].push(...user.purchases);
+        return acc;
+    }, {})
+);
+    // .map(([name, purchases]) => ({
+    //     name,
+    //     purchases
+    // }))
+    // .filter(arr => {
+    //     const itemCount = arr.purchases.length;
+    //     return itemCount > 2;
+    // })
+    // .map(arr => {
+    //     const totalSpending = arr.purchases.reduce((sum, n) => sum + n, 0);
+    //     return {...arr, totalSpending};
+    // })
+    // .sort((a, b) => b.totalSpending - a.totalSpending)
+    // .map(arr => ({
+    //     ...arr,
+    //     name : arr.name.toUpperCase()
+    // }))
+    // .slice(0,3)
+
+
+console.log(topBuyers(users));
+
+
+const transactions = [
+    { user: "husni", amount: 50,  type: "credit", timestamp: 101 },
+    { user: "ani", amount: 5,   type: "debit",  timestamp: 105 },
+    { user: "ramanda", amount: 200, type: "credit", timestamp: 110 },
+    { user: "ani",     amount: 100, type: "credit", timestamp: 120 },
+    { user: "rafi",    amount: 20,  type: "credit", timestamp: 99  },
+    { user: "ani",     amount: 15,  type: "debit",  timestamp: 121 },
+    { user: "husni",   amount: 200, type: "credit", timestamp: 130 },
+    { user: "rafi",    amount: 10,  type: "debit",  timestamp: 115 },
+    { user: "ani",     amount: 500, type: "credit", timestamp: 150 }
+];
+
+
+
+const detectFrauds = transactions =>
+    Object.entries(
+    transactions
+    .reduce((acc, transaction)=>{
+        const key = transaction.user;
+        if (!acc[key]){
+            acc[key] = [];
+        }
+        const {user, ...itemWithoutKey} = transaction;
+        acc[key].push(itemWithoutKey);
+        return acc;
+    },{}))
+    .map(([user, transaction]) => ({
+        user,
+        net : [...transaction]
+        .sort((a,b) => b.timestamp - a.timestamp)
+        .slice(0,3)
+        .reduce((sum, tx) =>
+            tx.type === "credit"
+                ? sum + tx.amount
+                : sum - tx.amount
+                ,0
+        )
+    }))
+    .filter(user => user.net > 100)
+    .sort((a,b)=> b.net - a.net)
+    .slice(0,3);
+console.log(JSON.stringify(detectFrauds(transactions), null, 2));
+
+
+
+
+
+    
+
